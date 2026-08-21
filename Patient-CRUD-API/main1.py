8947fab13d458e993c6031854debc7986749b9b0
Gauresh Chaubey <2k23.cscys2311078@gmail.com>
2026-08-21 23:29:17 +0530

    Learn query parameters

--- Patient-CRUD-API/main1.py
+++ Patient-CRUD-API/main1.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException,Path
 import json
 
 app = FastAPI()
@@ -18,3 +18,9 @@ def about():
 def view():
     data=load_data()
     return data
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str = Path(...,description="The ID of the patient to retrieve",examples="P001")):
+    data=load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail=f"Patient with ID {patient_id} not found")
