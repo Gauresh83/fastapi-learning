044e2284a957d2d8720731508b043f42c24f0082
Gauresh Chaubey <2k23.cscys2311078@gmail.com>
2026-08-22 00:00:38 +0530

    Learn query parameters

--- Patient-CRUD-API/main1.py
+++ Patient-CRUD-API/main1.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException,Path
+from fastapi import FastAPI, HTTPException,Path, Query
 import json
 
 app = FastAPI()
@@ -24,3 +24,14 @@ def view_patient(patient_id:str = Path(...,description="The ID of the patient to
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail=f"Patient with ID {patient_id} not found")
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc')):
+    data=load_data()
+    valid_fields = ['height', 'weight', 'bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field selected from {valid_fields}')
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order selected. Choose "asc" or "desc".')
+    sort_order = True if order == 'desc' else False
+    sorted_data= sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
